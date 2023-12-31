d40b1d90976e88ce8da65cb69f764bbdc699b315
josh-l-wang <josh@jlwang.org>
2023-12-30 22:00:04 -0500

    Update keymap.c

--- keyboards/tominabox1/le_chiffre/keymaps/vial/keymap.c
+++ keyboards/tominabox1/le_chiffre/keymaps/vial/keymap.c
@@ -74,9 +74,11 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 #define LED_CENTER_TOP 9
 #define LED_CENTER_BOTTOM 10
 
+/* sporkus
 #define LAYER_R layer_colors[layer][0] *  RGB_INDICATOR_BRIGHTNESS / 255
 #define LAYER_G layer_colors[layer][1] *  RGB_INDICATOR_BRIGHTNESS / 255
 #define LAYER_B layer_colors[layer][2] *  RGB_INDICATOR_BRIGHTNESS / 255
+*/
 
 #define MODS_ACTIVE(mods) \
     ((get_mods()|get_oneshot_mods()) & MOD_MASK_##mods ? RGB_INDICATOR_BRIGHTNESS:0)
@@ -85,6 +87,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 #define MODS_G MODS_ACTIVE(GUI) + SHIFT_ACTIVE
 #define MODS_B MODS_ACTIVE(ALT) + SHIFT_ACTIVE
 
+/*sporkus
 const uint8_t PROGMEM layer_colors[][4] = {
     {RGB_OFF},
     {RGB_RED},
@@ -96,26 +99,85 @@ const uint8_t PROGMEM layer_colors[][4] = {
     {RGB_TEAL}
 };
 
+
 void set_rgb_matrix_indicators(uint8_t led_min, uint8_t led_max) {
     #if defined(RGB_LAYER_INDICATOR_ENABLE)
     int layer = get_highest_layer(layer_state|default_layer_state);
     RGB_MATRIX_INDICATOR_SET_COLOR(LED_CENTER_TOP, LAYER_R, LAYER_G, LAYER_B);
-    /* uprintf("layer RGB: (%u, %u, %u)\n", LAYER_R, LAYER_G, LAYER_B); */
     #else
     RGB_MATRIX_INDICATOR_SET_COLOR(LED_CENTER_TOP, 0, 0, 0);
     #endif
 
     #if defined(RGB_MODS_INDICATOR_ENABLE)
     RGB_MATRIX_INDICATOR_SET_COLOR(LED_CENTER_BOTTOM, MODS_R, MODS_G, MODS_B);
-    /* uprintf("mod RGB: (%u, %u, %u)\n", MODS_R, MODS_G, MODS_B); */
     #else
     RGB_MATRIX_INDICATOR_SET_COLOR(LED_CENTER_BOTTOM, 0, 0, 0);
     #endif
 }
 
+
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     set_rgb_matrix_indicators(led_min, led_max);
     return false;
 }
+*/
+
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+  #if defined(RGB_MODS_INDICATOR_ENABLE)
+    RGB_MATRIX_INDICATOR_SET_COLOR(LED_CENTER_BOTTOM, MODS_R, MODS_G, MODS_B);
+    /* uprintf("mod RGB: (%u, %u, %u)\n", MODS_R, MODS_G, MODS_B); */
+  #else
+    RGB_MATRIX_INDICATOR_SET_COLOR(LED_CENTER_BOTTOM, 0, 0, 0);
+  #endif
+
+  if (rgb_matrix_is_enabled()){
+    HSV hsv = {0, 255, 0};
+    if (!host_keyboard_led_state().caps_lock) {
+        switch (get_highest_layer(layer_state|default_layer_state)) {
+            case 1:
+                hsv.h = 0;
+                hsv.v = 255;
+                break;
+            case 2:
+                hsv.h = 85;
+                hsv.v = 255;
+                break;
+            case 3:
+                hsv.h = 171;
+                hsv.v = 255;
+                break;
+            case 4:
+                hsv.h = 43;
+                hsv.v = 255;
+                break;
+            case 5:
+                hsv.h = 128;
+                hsv.v = 255;
+                break;
+            case 6:
+                hsv.h = 213;
+                hsv.v = 255;
+                break;
+            default: //  for any other layers, or the default layer
+                hsv.h = 0;
+                hsv.v = 0;
+                break;
+            };
+    }
+    if (host_keyboard_led_state().caps_lock) {
+        hsv.s = 0;
+        hsv.v = 255;
+    };
+    if (hsv.v > rgb_matrix_get_val()) {
+        hsv.v = rgb_matrix_get_val();
+    }
+    RGB rgb = hsv_to_rgb(hsv);
+    rgb_matrix_set_color(9, rgb.r, rgb.g, rgb.b);
+  }
+    return true;
+}
 
 #endif
+
+
+
