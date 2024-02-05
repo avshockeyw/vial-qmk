6e400555e991970713c8c4d4f2afd301848e7b49
josh-l-wang <josh@jlwang.org>
2024-02-05 13:36:47 -0500

    complete firmware for scandivan

--- keyboards/jlw/scandivan_v2/keymaps/vial/config.h
+++ keyboards/jlw/scandivan_v2/keymaps/vial/config.h
@@ -2,7 +2,10 @@
 
 #pragma once
 
-#define VIAL_KEYBOARD_UID {0x53, 0x9A, 0x09, 0xEC, 0x67, 0x0C, 0xBA, 0x7F}
+#define VIAL_KEYBOARD_UID {0x0D, 0xEB, 0x54, 0x1B, 0x78, 0x27, 0x05, 0x95}
 
 #define VIAL_COMBO_ENTRIES 69
 #define DYNAMIC_KEYMAP_LAYER_COUNT 8
+
+#define VIAL_UNLOCK_COMBO_ROWS { 1, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 9, 11 }
