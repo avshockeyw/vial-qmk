3f3c9260a70b7a7c782273d4ac60801c62993a46
avshockeyw <avshockeyw@users.noreply.github.com>
2024-03-05 11:21:38 -0500

    initial commit

---
+++ keyboards/jlw/le_chiffre_32/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define ENCODER_MAP_ENABLE
+
+// Comment these out if you don't want front indicators leds
+#define RGB_MODS_INDICATOR_ENABLE
+#define RGB_LAYER_INDICATOR_ENABLE
+
+#define RGB_INDICATOR_BRIGHTNESS 5
+#define RGB_TRIGGER_ON_KEYDOWN
