fabd0d37c8991f5d55f10a3d49cc81318424cf6a
avshockeyw <avshockeyw@users.noreply.github.com>
2024-03-22 13:17:35 -0400

    edit milk to work with keybit stm32 controller

--- keyboards/spaceman/2_milk/keymaps/vial/rules.mk
+++ keyboards/spaceman/2_milk/keymaps/vial/rules.mk
@@ -1,8 +1,7 @@
-MOUSEKEY_ENABLE     = no      # Mouse keys
+MOUSEKEY_ENABLE     = yes      # Mouse keys
 RGBLIGHT_ENABLE     = yes     # Enable WS2812 RGB underlight.
 VIA_ENABLE          = yes     # Enable VIA
 VIAL_ENABLE         = yes     # Enable VIAL
-OLED_ENABLE         = no
 LTO_ENABLE          = yes
-QMK_SETTINGS        = no
+QMK_SETTINGS        = yes
 COMBO_ENABLE        = yes
